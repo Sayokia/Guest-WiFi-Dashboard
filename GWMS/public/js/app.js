14bb9602430730998b759e7780c70e49f4156425
Yanlin.Zhu <740228019@qq.com>
2020-12-07 21:33:29 -0400

    add .env for development environment convinence

--- GWMS/public/js/app.js
+++ GWMS/public/js/app.js
@@ -63331,8 +63331,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/yanlinzhu/Desktop/csci3172-groupproject-m3/GWMS/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/yanlinzhu/Desktop/csci3172-groupproject-m3/GWMS/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Applications/MAMP/htdocs/csci3172-groupproject-m3/GWMS/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/csci3172-groupproject-m3/GWMS/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
