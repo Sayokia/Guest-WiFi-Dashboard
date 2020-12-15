ac4e980fa1f5369d7be5bb6c34334bf93c351672
Hugo Cheng <chengxinhai@HugodeiMac-Pro.local>
2020-12-15 19:49:56 -0400

    Fix dashboard store management UI

--- resources/views/stores/management.blade.php
+++ resources/views/stores/management.blade.php
@@ -144,32 +144,32 @@
                                 <div class="card-body">
                                     <div class="tab-content" id="myTabContent">
                                         <div class="tab-pane fade show active" id="ance_en" role="tabpanel" aria-labelledby="tabs-icons-text-1-tab">
-                                            <div class="form-group{{ $errors->has('name_en') ? ' has-danger' : '' }}">
+                                            <div class="form-group{{ $errors->has('ance_en') ? ' has-danger' : '' }}">
                                                 <label class="form-control-label" for="input-name">{{ __('Store Announcement in English') }}</label>
-                                                <input type="text" name="name_en" id="input-name_en" class="form-control form-control-alternative{{ $errors->has('name_en') ? ' is-invalid' : '' }}" placeholder="{{ __('Your Store Announcement in English') }}" value="{{ old('name_en', $info->name_en) }}" required autofocus>
+                                                <input type="text" name="ance_en" id="input-name_en" class="form-control form-control-alternative{{ $errors->has('ance_en') ? ' is-invalid' : '' }}" placeholder="{{ __('Your Store Announcement in English') }}" value="{{ old('ance_en', $info->ance_en) }}" required autofocus>
 
-                                                @if ($errors->has('name_en'))
+                                                @if ($errors->has('ance_en'))
                                                     <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('name_en') }}</strong>
+                                            <strong>{{ $errors->first('ance_en') }}</strong>
                                         </span>
                                                 @endif
                                             </div>
                                         </div>
                                         <div class="tab-pane fade" id="ance_zh" role="tabpanel" aria-labelledby="tabs-icons-text-2-tab">
-                                            <div class="form-group{{ $errors->has('name_zh') ? ' has-danger' : '' }}">
+                                            <div class="form-group{{ $errors->has('ance_zh') ? ' has-danger' : '' }}">
                                                 <label class="form-control-label" for="input-name">{{ __('Store Announcement in Chinese') }}</label>
-                                                <input type="text" name="name_zh" id="input-name_zh" class="form-control form-control-alternative{{ $errors->has('name_zh') ? ' is-invalid' : '' }}" placeholder="{{ __('Your Store Announcement in Chinese') }}" value="{{ old('name_en', $info->name_zh) }}" required autofocus>
+                                                <input type="text" name="name_zh" id="input-name_zh" class="form-control form-control-alternative{{ $errors->has('ance_zh') ? ' is-invalid' : '' }}" placeholder="{{ __('Your Store Announcement in Chinese') }}" value="{{ old('ance_zh', $info->ance_zh) }}" required autofocus>
 
-                                                @if ($errors->has('name_zh'))
+                                                @if ($errors->has('ance_zh'))
                                                     <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('name_zh') }}</strong>
+                                            <strong>{{ $errors->first('ance_zh') }}</strong>
                                         </span>
                                                 @endif
                                             </div>                            </div>
                                         <div class="tab-pane fade" id="ance_fr" role="tabpanel" aria-labelledby="tabs-icons-text-3-tab">
-                                            <div class="form-group{{ $errors->has('name_fr') ? ' has-danger' : '' }}">
+                                            <div class="form-group{{ $errors->has('ance_fr') ? ' has-danger' : '' }}">
                                                 <label class="form-control-label" for="input-name">{{ __('Store Announcement in French') }}</label>
-                                                <input type="text" name="name_fr" id="input-name_fr" class="form-control form-control-alternative{{ $errors->has('name_fr') ? ' is-invalid' : '' }}" placeholder="{{ __('Your Store Announcement in French') }}" value="{{ old('name_en', $info->name_fr) }}" required autofocus>
+                                                <input type="text" name="ance_fr" id="input-name_fr" class="form-control form-control-alternative{{ $errors->has('ance_fr') ? ' is-invalid' : '' }}" placeholder="{{ __('Your Store Announcement in French') }}" value="{{ old('ance_fr', $info->ance_fr) }}" required autofocus>
 
                                                 @if ($errors->has('name_fr'))
                                                     <span class="invalid-feedback" role="alert">
@@ -178,9 +178,9 @@
                                                 @endif
                                             </div>                            </div>
                                         <div class="tab-pane fade" id="ance_jp" role="tabpanel" aria-labelledby="tabs-icons-text-3-tab">
-                                            <div class="form-group{{ $errors->has('name_jp') ? ' has-danger' : '' }}">
+                                            <div class="form-group{{ $errors->has('ance_jp') ? ' has-danger' : '' }}">
                                                 <label class="form-control-label" for="input-name">{{ __('Store Announcement in Japanese') }}</label>
-                                                <input type="text" name="name_jp" id="input-name_jp" class="form-control form-control-alternative{{ $errors->has('name_jp') ? ' is-invalid' : '' }}" placeholder="{{ __('Your Store Announcement in Japaneses') }}" value="{{ old('name_jp', $info->name_jp) }}" required autofocus>
+                                                <input type="text" name="ance_jp" id="input-ance_jp" class="form-control form-control-alternative{{ $errors->has('ance_jp') ? ' is-invalid' : '' }}" placeholder="{{ __('Your Store Announcement in Japaneses') }}" value="{{ old('ance_jp', $info->ance_jp) }}" required autofocus>
 
                                                 @if ($errors->has('name_jp'))
                                                     <span class="invalid-feedback" role="alert">
@@ -189,9 +189,9 @@
                                                 @endif
                                             </div>                            </div>
                                         <div class="tab-pane fade" id="ance_kr" role="tabpanel" aria-labelledby="tabs-icons-text-3-tab">
-                                            <div class="form-group{{ $errors->has('name_kr') ? ' has-danger' : '' }}">
+                                            <div class="form-group{{ $errors->has('ance_kr') ? ' has-danger' : '' }}">
                                                 <label class="form-control-label" for="input-name">{{ __('Store Announcement in Korean') }}</label>
-                                                <input type="text" name="name_kr" id="input-name_kr" class="form-control form-control-alternative{{ $errors->has('name_kr') ? ' is-invalid' : '' }}" placeholder="{{ __('Your Store Announcement in Korean') }}" value="{{ old('name_kr', $info->name_kr) }}" required autofocus>
+                                                <input type="text" name="ance_kr" id="input-ance_kr" class="form-control form-control-alternative{{ $errors->has('ance_kr') ? ' is-invalid' : '' }}" placeholder="{{ __('Your Store Announcement in Korean') }}" value="{{ old('ance_kr', $info->ance_kr) }}" required autofocus>
 
                                                 @if ($errors->has('name_en'))
                                                     <span class="invalid-feedback" role="alert">
